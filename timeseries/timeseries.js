867e702c0ec7e3ffbade5e367a68269ea1965b14
NColes <colesniall@gmail.com>
2016-07-11 13:38:21 +0100

    Smarter sorting, paging timeseries
    Added a hidden column that creates a yyyymmdd format date which makes sorting more sensible. Plugin added to move text values to the end.

--- timeseries/timeseries.js
+++ timeseries/timeseries.js
@@ -1,7 +1,3 @@
-var myJSON;
-var jsonDate;
-var jsonMonth;
-var jsonYear;
 var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var dToday = new Date;
 var dateToday = dToday.getDate();
@@ -12,61 +8,125 @@ var yearToday = yToday.getFullYear();
 
 var dayToday = dateToday + ' ' + monthToday + ' ' + yearToday;
 
-
+var dateArray = [];
+var jsonDate;
 //Get JSON data, loop through all results combining the title, URI and release date with HTML table formatting
-$.getJSON("https://www.ons.gov.uk/search/data?q=AM+BB+BBSU+BERD+CAPSTK+CT+CXNV+DIOP+DRSI+EMP+IOS1+LMS+MM19+MM22+MM23+MQ10+MQ5+MRET+NFBS+OTT+PB+PGDP+PN2+PNBP+PPI+PRDY+PROF+PSE+PUSF+QNA+RGHI+SDQ7+SPPI+SRS+TOPSI+UKEA+UNEM&sortBy=relevance&filter=datasets&q=AM+BB+BBSU+BERD+CAPSTK+CT+CXNV+DIOP+DRSI+EMP+IOS1+LMS+MM19+MM22+MM23+MQ10+MQ5+MRET+NFBS+OTT+PB+PGDP+PN2+PNBP+PPI+PRDY+PROF+PSE+PUSF+QNA+RGHI+SDQ7+SPPI+SRS+TOPSI+UKEA+UNEM&size=50", function(json) {
+$.getJSON("https://www.ons.gov.uk/search/data?q=AM+BB+BBSU+BERD+CAPSTK+CT+CXNV+DIOP+DRSI+EMP+IOS1+LMS+MM19+MM22+MM23+MQ10+MQ5+MRET+NFBS+OTT
+
++PB+PGDP+PN2+PNBP+PPI+PRDY+PROF+PSE+PUSF+QNA+RGHI+SDQ7+SPPI+SRS+TOPSI+UKEA+UNEM&sortBy=relevance&filter=datasets&q=AM+BB+BBSU+BERD+CAPSTK+CT
+
++CXNV+DIOP+DRSI+EMP+IOS1+LMS+MM19+MM22+MM23+MQ10+MQ5+MRET+NFBS+OTT+PB+PGDP+PN2+PNBP+PPI+PRDY+PROF+PSE+PUSF+QNA+RGHI+SDQ7+SPPI+SRS+TOPSI+UKEA
+
++UNEM&size=50", function(json) {
     JSON = json;
 
     function myFunction(arr) {
 
+
+
         var JSONout = "";
-        var i;
-	var jsonTitle;
-	var jsonTitle2;
-	var jsonTitle3;
+
         console.log(arr[0].result.results[0].description.title);
         console.log(arr[0].result.results[0].uri);
         console.log(arr[0].result.results[0].description.releaseDate);
         console.log(arr[0].result.results.length);
 
 
-        for (i = 0; i < arr[0].result.results.length; i++) {
-if (arr[0].result.results[i].description.nextRelease === "" || arr[0].result.results[i].description.nextRelease === null) {
-	jsonDate = "Not yet announced";
-} else {
+        for (var i = 0; i < arr[0].result.results.length; i++) {
 
-	jsonDate = arr[0].result.results[i].description.nextRelease;
-};
-	    jsonTitle = arr[0].result.results[i].description.title;
-	    jsonTitle2 = jsonTitle.split("<strong>").join("");
-	    jsonTitle3 = jsonTitle2.split("</strong>").join("");
-
-	
-            //Highlight todays releases
-
-            if (jsonDate === dayToday) {
-                JSONout += '<tr style="color:red;"><td><a href="https://www.ons.gov.uk' + arr[0].result.results[i].uri + '">' + jsonTitle3 + '</a></td>' + '<td class="tableData">' + jsonDate + '</td></tr>';
+            if (arr[0].result.results[i].description.nextRelease === "") {
+                jsonDate = ("Not yet announced");
             } else {
-                JSONout += '<tr><td><a href="https://www.ons.gov.uk' + arr[0].result.results[i].uri + '">' + jsonTitle3 + '</a></td>' + '<td class="tableData">' + jsonDate + '</td></tr>';
-            };
 
-            document.getElementById("publishedreleases").innerHTML = JSONout;
+                jsonDate = arr[0].result.results[i].description.nextRelease;
+            };
+            jsonTitle = arr[0].result.results[i].description.title;
+            jsonTitle2 = jsonTitle.split("<strong>").join("");
+            jsonTitle3 = jsonTitle2.split("</strong>").join("");
+
+            if (arr[0].result.results[i].description.nextRelease != null) {
+                dateArray = jsonDate.split(" ");
+                dateArray = dateArray.filter(function(e) {
+                    return e.replace(/(\r\n|\n|\r)/gm, "")
+                });
+                if (dateArray[0] > 0) {
+                    if (dateArray[0] < 10) {
+                        dateArray[0] = "0" + dateArray[0];
+                    };
+                    //			dateFormatted = dateArray[0].concat("/", monthNames.indexOf(dateArray[1]) + 1, "/", dateArray[2]);
+                    dateFormatted = dateArray[2].concat(monthNames.indexOf(dateArray[1]) + 1, dateArray[0]);
+                    console.log(dateFormatted);
+                    //			console.log(dateArray[0]);
+                    //			console.log(monthNames.indexOf(dateArray[1]) + 1);
+                    //			console.log(dateArray[2]);
+                };
+
+
+                //Highlight todays releases
+
+                if (jsonDate === dayToday) {
+                    JSONout += '<tr style="color:red;"><td><a href="https://www.ons.gov.uk' + arr[0].result.results[i].uri + '">' + 
+
+jsonTitle3 + '</a></td>' + '<td class="tableData">' + jsonDate + '</td><td>' + dateFormatted + '</td></tr>';
+                } else {
+                    JSONout += '<tr><td><a href="https://www.ons.gov.uk' + arr[0].result.results[i].uri + '">' + jsonTitle3 + '</a></td>' + 
+
+'<td class="tableData">' + jsonDate + '</td><td>' + dateFormatted + '</td></tr>';
+                };
+
+                document.getElementById("publishedreleases").innerHTML = JSONout;
+                dateFormatted = "";
+
 
             };
-document.body.innerHTML = document.body.innerHTML.replace('undefined', 'Not yet announced');
+            document.body.innerHTML = document.body.innerHTML.replace('undefined', 'Not yet announced');
+
+
+
         };
-
-
+    };
 
     myFunction([
         JSON
     ]);
-    $(document).ready(function() {
-    $('#chartTable').DataTable( {
-        "order": [[ 0, "asc" ]],         
-"bLengthChange": false,
-"paging":false
-    } );
-} );
+
+
+    function sortNumbersIgnoreText(a, b, high) {
+        var reg = /[+-]?((\d+(\.\d*)?)|\.\d+)([eE][+-]?[0-9]+)?/;
+        a = a.match(reg);
+        a = a !== null ? parseFloat(a[0]) : high;
+        b = b.match(reg);
+        b = b !== null ? parseFloat(b[0]) : high;
+        return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+    }
+    jQuery.extend(jQuery.fn.dataTableExt.oSort, {
+        "sort-numbers-ignore-text-asc": function(a, b) {
+            return sortNumbersIgnoreText(a, b, Number.POSITIVE_INFINITY);
+        },
+        "sort-numbers-ignore-text-desc": function(a, b) {
+            return sortNumbersIgnoreText(a, b, Number.NEGATIVE_INFINITY) * -1;
+        }
+    });
+
+    $('#chartTable').dataTable({
+	"pageLength": 20,
+        "order": [
+            [2, "asc"],
+            [1, "asc"]
+        ],
+
+        "columnDefs": [{
+            type: 'sort-numbers-ignore-text',
+            targets: 2
+        }, {
+            "visible": false,
+            "targets": 2
+        }, {
+            "iDataSort": 2,
+            "aTargets": 1
+        }]
+
+    });
+
 
 });
