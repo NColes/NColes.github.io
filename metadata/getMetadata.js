6f1707bdcc6e7f3ce6ac393b0a8520679d3bf1b8
NColes <colesniall@gmail.com>
2017-02-10 14:50:49 +0000

    Update getMetadata.js

--- metadata/getMetadata.js
+++ metadata/getMetadata.js
@@ -105,11 +105,13 @@ getRelatedData = function(callback) {
                 } else if (json.description.nationalStatistic === false) {
                     NSStatus = "&#10007; No";
                 };
-
-                if (json.hasOwnProperty.section.markdown === "") {
-                    timeseriesNotes = "No notes.";
+                
+                if (json.section.hasOwnProperty("markdown")) {
+                    if (json.sectionmarkdown != "") {
+                        timeseriesNotes = (json.section.markdown).replace(/\n/g, "<br>");
+                    };      
                 } else {
-                    timeseriesNotes = (json.hasOwnProperty.section.markdown).replace(/\n/g, "<br>");
+                    timeseriesNotes = "No notes.";
                 };
                 relData = '<table class="table table-hover"><thead><tr><td colspan="2"><h4 style="padding-left:0px;"><a href="https://www.ons.gov.uk' + json.uri + '" target="_blank">' + json.description.title + '</a></h4></td></tr></thead><tbody><tr><td style="width:130px;">Summary</td><td>' + json.description.summary + '</td></tr><tr><td>Contact</td><td>' + json.description.contact.name + '</br>' + json.description.contact.email + '</br>' + json.description.contact.telephone + '</td></tr><tr><td>Dataset ID</td><td>' + json.description.datasetId + '</td></tr><tr><td>National statistic</td><td>' + NSStatus + '</td></tr><tr><td>Keywords</td><td>' + TSDKeywords + '</td></tr><tr><td>Metadescription</td><td>' + json.description.metaDescription + '</td></tr><tr><td>Notes</td><td>' + timeseriesNotes + '</td></tr></tbody></table>';
                 TSDArray.push(relData);
