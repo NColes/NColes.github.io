fd2e451215973b9efab184b93c884f940a0de432
NColes <colesniall@gmail.com>
2017-05-04 16:22:05 +0100

    Update getMetadata.js
    Fix for UTC time issues with last release date.

--- metadata/getMetadata.js
+++ metadata/getMetadata.js
@@ -16,7 +16,11 @@ basicMetadata = function() {
     document.getElementById("dName").innerHTML = myJSON.description.contact.name;
     document.getElementById("dPhone").innerHTML = myJSON.description.contact.telephone;
     document.getElementById("dNext").innerHTML = myJSON.description.nextRelease;
-    document.getElementById("dPubl").innerHTML = myJSON.description.releaseDate.substring(8, 10) + " " + monthNames[myJSON.description.releaseDate.substring(5, 7) - 1] + " " + myJSON.description.releaseDate.substring(0, 4);
+    if ((new Date).getTimezoneOffset() === -60) {
+        document.getElementById("dPubl").innerHTML = myJSON.description.releaseDate.substring(8, 10) + " " + monthNames[myJSON.description.releaseDate.substring(5, 7) - 1] + " " + myJSON.description.releaseDate.substring(0, 4);    
+    } else {
+        document.getElementById("dPubl").innerHTML = (Number(myJSON.description.releaseDate.substring(8, 10)) + 1) + " " + monthNames[myJSON.description.releaseDate.substring(5, 7) - 1] + " " + myJSON.description.releaseDate.substring(0, 4);
+    }
 
     if (myJSON.type === "article") {
         document.getElementById("dSumm").innerHTML = myJSON.description._abstract;
