8efaa2244ed98c414331df819e9399ed4e464ed1
NColes <colesniall@gmail.com>
2017-05-08 10:43:47 +0100

    Add files via upload

---
+++ releasesheet/published.js
@@ -0,0 +1,27 @@
+generatePublishedTable( function() {
+    $('#publishedTable').dataTable({
+        "lengthChange": false,
+        "pageLength": 15,
+        "searching": false,
+        "autoWidth": false,
+        "order": [
+            [3, "desc"]
+        ],
+        "columnDefs": [{
+            "targets": 3,
+            "visible": false
+        }],
+        "drawCallback": function(settings) {
+            $('[data-toggle="tooltip"]').tooltip();
+
+        }
+    });
+bindPublishedButtons();
+resizePublished();
+});
+
+setTimeout(resizePublished, 6000);
+
+document.body.onresize = function() {
+	resizePublished()
+};
